d2d0f039e95ac44fdedb71969d8679816142ea54
smadzelonka <smadzelonka@gmail.com>
2021-02-22 17:43:57 -0800

    basic layout down

---
+++ frontend/src/pages/landingPage/LandingPage.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  const [state, setState] = useState("");
+  return (
+    <>
+      <Container className="LandingPageContainer">
+        <Row className="LandingPageRow">
+          <Col className="LandingPageLeft">
+            <Container>
+              <h2>Follow your interests</h2>
+              <h2>Follow your interests</h2>
+              <h2>Follow your interests</h2>
+            </Container>
+          </Col>
+          <Col className="LandingPageRight">
+            <Container>
+              <h2>See what's Happening in the world right now</h2>
+            </Container>
+          </Col>
+        </Row>
+        <Navbar fixed="bottom" className="LandingPageNav">
+          <Nav>
+            <Nav.Link href="#features" className="LandingPageLink">
+              Features
+            </Nav.Link>
+            <Nav.Link href="#features" className="LandingPageLink">
+              Features
+            </Nav.Link>
+            <Nav.Link href="#features" className="LandingPageLink">
+              Features
+            </Nav.Link>
+            <Nav.Link href="#features" className="LandingPageLink">
+              Features
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+      </Container>
+    </>
+  );
+};
+
+export default LandingPage;
