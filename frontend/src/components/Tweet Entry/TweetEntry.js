5916a2764aa8c7109dd0badb41c43b833c4f1d47
Dan <neveloffd@gmail.com>
2021-02-24 14:14:53 -0500

    added tweet entry

---
+++ frontend/src/components/Tweet Entry/TweetEntry.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { faUserCircle, faRetweet } from "@fortawesome/free-solid-svg-icons";
+import {
+  faComment,
+  faHeart,
+  faShareSquare,
+} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Tweets.css";
+
+function TweetEntry() {
+  return (
+    <Card>
+      <Container>
+        <Row>
+          <Col xs={2}>
+            <FontAwesomeIcon
+              className="image-icon"
+              icon={faUserCircle}
+              size="4x"
+            />
+          </Col>
+          <Col>
+            <Card.Body>
+              <Card.Title className="username">Elon Musk</Card.Title>
+              <Card.Subtitle className="tweet-title mb-2 text-muted">
+                @ElonMusk
+              </Card.Subtitle>
+              <Card.Subtitle className="tweet-title mb-2 text-muted">
+                7m
+              </Card.Subtitle>
+              <Card.Text className="text-left">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <Row>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faComment}
+                    size="1x"
+                  />
+                  {/* TODO Comment counter */}
+                  <Card.Subtitle className="tweet-title mb-2 text-muted">
+                    7
+                  </Card.Subtitle>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faRetweet}
+                    size="1x"
+                  />
+                  {/* TODO Retweet Counter */}
+                  <Card.Subtitle className="tweet-title mb-2 text-muted">
+                    3
+                  </Card.Subtitle>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faHeart}
+                    size="1x"
+                  />
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faShareSquare}
+                    size="1x"
+                  />
+                </Card.Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
+  );
+}
+
+export default TweetEntry;
