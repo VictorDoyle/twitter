78e294eb7ade687c2e3fef7108a72fe17e21a7d1
smadzelonka <smadzelonka@gmail.com>
2021-02-23 18:26:03 -0800

    really poops data picker added in should revision it but in hurry

--- frontend/src/pages/landingPage/LandingPage.js
+++ frontend/src/pages/landingPage/LandingPage.js
@@ -1,15 +1,45 @@
 import React, { useState } from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import Modal from "../../components/LandingPage/Modal";
-import { Container, Row, Col, Navbar, Nav, Button } from "react-bootstrap";
+import Modal from "../../components/Modal";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  Button,
+  Form,
+} from "react-bootstrap";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   const [state, setState] = useState("");
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = (e) => {
+    e.preventDefault();
+    setShow(false);
+  };
   const handleShow = () => setShow(true);
+
+  const months = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
+
+  const year = new Date().getFullYear();
+  const years = Array.from(new Array(50), (val, index) => index - year);
+  const days = Array.from(Array(31 + 1).keys());
   return (
     <>
       <Container
@@ -60,7 +90,7 @@ const LandingPage = () => {
           </Nav>
         </Navbar>
       </Container>
-
+      {/* modal */}
       <Modal
         show={show}
         handleClose={handleShow}
@@ -68,7 +98,9 @@ const LandingPage = () => {
           <Container>
             <Row>
               <Col xs={6} md={4}></Col>
-              <Col xs={6} md={4}></Col>
+              <Col xs={6} md={4}>
+                Logo
+              </Col>
               <Col xs={6} md={4}>
                 <Button
                   variant="primary"
@@ -81,8 +113,47 @@ const LandingPage = () => {
             </Row>
           </Container>
         }
-        body="hello World"
-        footer="Woohoo"
+        body={
+          <>
+            <h3>Create your account</h3>
+            <Form>
+              <Form.Control type="name" placeholder="Name" />
+              <Form.Control type="phone" placeholder="Phone" />
+              <p>Maybe Email?</p>
+              <h6>Date of birth</h6>
+              <Form.Row>
+                <Col xs={6}>
+                  <Form.Control as="select" type="month">
+                    {months.map((x, i) => (
+                      <option key={`x${i}`} value={x}>
+                        {x}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Col>
+                <Col>
+                  <Form.Control as="select" type="day">
+                    {days.map((d, i) => (
+                      <option key={`d${i}`} value={d}>
+                        {d}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Col>
+                <Col>
+                  <Form.Control as="select" type="year">
+                    {years.map((y, i) => (
+                      <option key={`y${i}`} value={y}>
+                        {y}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Col>
+              </Form.Row>
+            </Form>
+          </>
+        }
+        footer=""
       />
     </>
   );
