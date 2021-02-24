cf40d75bc2d2e9e695064277497b20a0e21075ba
claywatkins8 <claywatkins8@gmail.com>
2021-02-24 13:51:00 -0800

    updates feed.js

--- frontend/src/pages/feed/Feed.js
+++ frontend/src/pages/feed/Feed.js
@@ -1,10 +1,12 @@
-import NavBar from "../components/NavBar/NavBar";
-import Tweets from "../components/Tweets/Tweets";
+import NavBar from "../../components/NavBar/NavBar";
+import Tweets from "../../components/Tweets/Tweets";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import TweetEntry from "../components/Tweet Entry/TweetEntry";
-import TweetEntryBefore from "../components/Tweet Entry/TweetEntryBefore";
+import TweetEntry from "../../components/Tweet Entry/TweetEntry";
+import TweetEntryBefore from "../../components/Tweet Entry/TweetEntryBefore";
+import WhatsHappening from "../../components/WhatsHappening/WhatsHappening";
+import WhoToFollow from "../../components/WhoToFollow/WhoToFollow";
 import "./Feed.css";
 import React, { useState } from "react";
 
@@ -36,7 +38,8 @@ function Feed() {
             <Tweets />
           </Col>
           <Col>
-            <section>WHATS HAPPENING</section>
+            <WhatsHappening />
+            <WhoToFollow />
           </Col>
         </Row>
       </Container>
