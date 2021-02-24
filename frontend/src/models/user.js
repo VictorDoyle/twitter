28475c6d221106e908e8062fc2a92485e4605a6a
smadzelonka <smadzelonka@gmail.com>
2021-02-24 15:40:56 -0800

    what in the actual fuck is going on

--- frontend/src/models/user.js
+++ frontend/src/models/user.js
@@ -1,18 +1,21 @@
-const url = `http://localhost:4000/api/users`
+import axios from "axios";
+
+const url = `http://localhost:4000/api/users`;
 
 class UserModel {
   // access all users
   static all = () => {
-    return fetch(`${url}`, {
-      method: "GET",
-      headers: {
-        authorization: `Bearer ${localStorage.uid}`
-      }
-    }).then(res => res.json())
-  }
+    return axios
+      .get(`${url}`, {
+        headers: {
+          authorization: `Bearer ${localStorage.uid}`,
+        },
+      })
+      .then((res) => res.json());
+  };
 
   // accessed current user via Id+login
- /*  static show = (id) => {
+  /*  static show = (id) => {
     return fetch(`${API}/verify`, {
         method: "GET",
         headers: {
@@ -22,54 +25,52 @@ class UserModel {
     }).then(res => res.json())
   } */
 
-
   // show specific user
   static showUser = (id) => {
     return fetch(`${url}/${id}`, {
-        method: "GET",
-        headers: {
-          authorization: `Bearer ${localStorage.uid}`
-        }
-
-    }).then(res => res.json())
-  }
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.uid}`,
+      },
+    }).then((res) => res.json());
+  };
 
   // create a user
   static create = (data) => {
-    return fetch(`${url}`, {
-      // options
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }).then(res => res.json())
-  } 
+    return axios
+      .post(`${url}`, {
+        // options
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+      .then((res) => res.data);
+  };
 
   // update-Edit user
 
-static update = (data) => {
-  return fetch(`${url}/edit`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      authorization: `Bearer ${localStorage.uid}`
-    },
-    body: JSON.stringify(data)
-  }).then(res => res.json())
-} 
-
+  static update = (data) => {
+    return fetch(`${url}/edit`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.uid}`,
+      },
+      body: JSON.stringify(data),
+    }).then((res) => res.json());
+  };
 
   // delete route for user
   static delete = (id) => {
-    console.log("showing deleted user", id)
+    console.log("showing deleted user", id);
     return fetch(`${url}/delete/${id}`, {
       method: "DELETE",
       headers: {
-        authorization: `Bearer ${localStorage.uid}`
-      }
-    }).then(res => res.json())
-  }
+        authorization: `Bearer ${localStorage.uid}`,
+      },
+    }).then((res) => res.json());
+  };
 }
 
-export default UserModel
+export default UserModel;
