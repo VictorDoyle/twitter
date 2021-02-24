a7983386ab85a0fb1fdad77b0f84b7cf3f19f930
claywatkins8 <claywatkins8@gmail.com>
2021-02-24 12:15:27 -0800

    whats happening format

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,22 +1,27 @@
-import NavBar from '../components/NavBar/NavBar'
-import Tweets from '../components/Tweets/Tweets'
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import NavBar from "../components/NavBar/NavBar";
+import Tweets from "../components/Tweets/Tweets";
+import WhatsHappening from "../components/WhatsHappening/WhatsHappening";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import "./Feed.css";
-
 
 function Feed() {
   return (
     <div className="Feed" id="feed-page">
-        <Container>
+      <Container>
         <Row>
-        <Col><NavBar /></Col>
-        <Col md={6}><Tweets /></Col>
-        <Col><section>WHATS HAPPENING</section></Col>
+          <Col className="col">
+            <NavBar />
+          </Col>
+          <Col className="col tweet-width" md={6}>
+            <Tweets />
+          </Col>
+          <Col className="col">
+            <WhatsHappening />
+          </Col>
         </Row>
-        </Container>
-        
+      </Container>
     </div>
   );
 }
