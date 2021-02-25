bb6d74e7b605a6721059b79b8aadf2b69ab5a7af
victordoyle <victor-doyle@outlook.com>
2021-02-25 17:44:13 -0500

    merged

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -3,8 +3,7 @@ import Tweets from "../../components/Tweets/Tweets";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import TweetEntry from "../../components/Tweet Entry/TweetEntry";
-import TweetEntryBefore from "../../components/Tweet Entry/TweetEntryBefore";
+
 import WhatsHappening from "../../components/WhatsHappening/WhatsHappening";
 import WhoToFollow from "../../components/WhoToFollow/WhoToFollow";
 
@@ -22,7 +21,79 @@ function Profile() {
           <Col md={6}>
         
 
-            <Tweets />
+          <Card>
+      <Container>
+        <Row>
+          <Col xs={2}>
+            <FontAwesomeIcon
+              className="image-icon"
+              icon={faUserCircle}
+              size="4x"
+            />
+          </Col>
+          <Col>
+            <Card.Body>
+              <Card.Title className="username">Elon Musk</Card.Title>
+              <Card.Subtitle className="tweet-title mb-2 text-muted">
+                {/* {props.tweet.author} */}
+              </Card.Subtitle>
+              <Card.Subtitle className="tweet-title mb-2 text-muted">
+                7m
+              </Card.Subtitle>
+              <Card.Text className="text-left">
+                {/* {props.tweet.description} */}
+              </Card.Text>
+            </Card.Body>
+            <Row>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faComment}
+                    size="1x"
+                  />
+                  {/* TODO Comment counter */}
+                  <Card.Subtitle className="tweet-title mb-2 text-muted">
+                    7
+                  </Card.Subtitle>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faRetweet}
+                    size="1x"
+                  />
+                  {/* TODO Retweet Counter */}
+                  <Card.Subtitle className="tweet-title mb-2 text-muted">
+                    3
+                  </Card.Subtitle>
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faHeart}
+                    size="1x"
+                  />
+                </Card.Link>
+              </Col>
+              <Col>
+                <Card.Link className="text-muted" href="#">
+                  <FontAwesomeIcon
+                    className="image-icon"
+                    icon={faShareSquare}
+                    size="1x"
+                  />
+                </Card.Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
           </Col>
           <Col>
             <WhatsHappening />
