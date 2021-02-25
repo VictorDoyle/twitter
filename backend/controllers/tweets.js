c8df53cd5811cd6429374f355f4663c838a8b126
Dan <neveloffd@gmail.com>
2021-02-25 00:55:12 -0500

    tweet routes set and server.js updated as such

---
+++ backend/controllers/tweets.js
@@ -0,0 +1,77 @@
+import express from "express";
+import prisma from "@prisma/client";
+const router = express.Router();
+
+const db = new prisma.PrismaClient({
+  log: ["info", "warn"],
+  errorFormat: "pretty",
+});
+
+/* SECTION: TWEET ROUTES */
+
+/* INDEX FOR ALL TWEETS */
+router.get("/", async function (request, response) {
+  const tweets = await db.tweet.findMany({
+    include: {
+      author: true,
+      description: true,
+      createdAt: true,
+      category: true,
+    },
+  });
+  response.json({ tweets });
+});
+
+/* SHOW ONE TWEET BY ID */
+router.get("/:id", async function (request, response) {
+  const tweet = await db.user.findUnique({
+    where: {
+      id: Number(request.params.id),
+    },
+    include: {
+      comments: {
+        select: {
+          content: true,
+          author: true,
+          createdAt: true,
+        },
+      },
+    },
+  });
+
+  response.json({ tweet });
+});
+
+/* CREATE TWEET */
+
+router.post("/", async function (request, response) {
+  const createdTweet = await db.tweet.create({
+    data: request.body,
+  });
+
+  response.json({ message: "The User was created", tweet: createdTweet });
+});
+
+/* DELETE ROUTE */
+
+router.delete("/delete/:id", async function (request, response) {
+  const deleteTweet = await db.tweet.delete({
+    where: {
+      id: Number(request.params.id),
+    },
+  });
+  const deleteAttachedComments = await db.comment.deleteMany({
+    where: {
+      tweetId: Number(request.params.id),
+    },
+  });
+
+  // message return on create for testing
+  response.json({
+    message: "the user and their posts have been deleted",
+    tweet: deleteTweet,
+    comments: deleteAttachedComments,
+  });
+});
+
+export default router;
