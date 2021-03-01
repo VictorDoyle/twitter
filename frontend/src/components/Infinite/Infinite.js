5e8cc17b5e056b0efdfa2969642f986bf2c6a6ec
Dan <neveloffd@gmail.com>
2021-02-28 19:53:15 -0500

    messing with DOOM SCROLLLLLLLLL

--- frontend/src/components/Infinite/Infinite.js
+++ frontend/src/components/Infinite/Infinite.js
@@ -2,50 +2,28 @@ import React, { useEffect, useState, useRef } from "react";
 import tweetModel from "../../models/tweet";
 import Tweets from "../../components/Tweets/Tweets";
 
-const divStyle = {
-  color: "blue",
-  height: "250px",
-  textAlign: "center",
-  padding: "5px 10px",
-  background: "#eee",
-  marginTop: "15px",
-};
-
-const containerStyle = {
-  maxWidth: "1280px",
-  margin: "0 auto",
-};
 const Infinite = () => {
   const [postList, setPostList] = useState([]);
-
-  // tracking on which page we currently are
   const [page, setPage] = useState(1);
-  // add loader refrence
-  const loader = useRef(null);
   const [tweets, setTweets] = useState([]);
+  let initialState = 0;
+  let initialState2 = 5;
+  const [currentIndexStart, setCurrentIndexStart] = useState(initialState);
+  const [currentIndexEnd, setCurrentIndexEnd] = useState(initialState2);
+  const loader = useRef(null);
   useEffect(function () {
     fetchData();
   }, []);
 
-  if (tweets) {
-    setPostList(tweets.slice(0, 5));
-    return (
-      <>
-        {postList.map((post, index) => {
-          return (
-            <div key={index} className="post" style={divStyle}>
-              <h2> {post} </h2>
-            </div>
-          );
-        })}{" "}
-      </>
-    );
-  }
+  const currentTweets = tweets.slice(0, 5);
+
+  /* setPostList(currentTweets); */
 
   const fetchData = () => {
     tweetModel.all().then((data) => {
       setTweets(data.tweets);
     });
+    setPostList(currentTweets);
   };
 
   useEffect(() => {
@@ -62,12 +40,16 @@ const Infinite = () => {
     }
   }, []);
 
+  console.log(postList);
+
   useEffect(() => {
     // here we simulate adding new posts to List
-    const newList = postList.concat([1, 1, 1, 1]);
-    setPostList({
-      list: newList,
-    });
+    // const newList = postList.concat(currentTweets);
+    if (currentTweets) {
+      setPostList(postList.push(currentTweets));
+      setCurrentIndexStart(initialState + 5);
+      setCurrentIndexEnd(initialState2 + 5);
+    }
   }, [page]);
 
   // here we handle what happens when user scrolls to Load More div
@@ -75,18 +57,43 @@ const Infinite = () => {
   const handleObserver = (entities) => {
     const target = entities[0];
     if (target.isIntersecting) {
-      setPage((page) => page + 1);
+      setPage((page) => page);
+    } else {
+      console.log("Vickies Titties");
     }
   };
 
+  /*   let allTweets = postList.map((tweet, index) => {
+    return (
+      <>
+        <Tweets {...tweet} key={tweet.id} />
+      </>
+    );
+  }); */
+
+  const displayTweets = () => {
+    tweets ? (
+      postList.map((tweet, index) => {
+        return (
+          <>
+            <Tweets {...tweet} key={tweet.id} />
+          </>
+        );
+      })
+    ) : (
+      <h1>Hello</h1>
+    );
+  };
+
   return (
-    <div className="container" style={containerStyle}>
-      <div className="post-list">
+    <>
+      <div className="container">
         <div className="loading" ref={loader}>
+          {displayTweets}
           <h2>Load More</h2>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
