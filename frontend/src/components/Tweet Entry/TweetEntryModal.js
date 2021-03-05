ce12737f2e59d36ab383cab34c36264f8904d0ec
Dan <neveloffd@gmail.com>
2021-03-04 19:06:50 -0500

    infinite scroll

---
+++ frontend/src/components/Tweet Entry/TweetEntryModal.js
@@ -0,0 +1,6 @@
+import Modal from "../Modal.js";
+import TweetEntry from "./TweetEntry";
+
+function TweetEntryModal({ submitHandler, description, descriptionValue }) {
+  return <Modal body={<TweetEntry />} />;
+}
