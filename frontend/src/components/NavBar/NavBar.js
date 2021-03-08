3c0c87e9c7f69d1bb57ef0342ebfd373bd9252dd
smadzelonka <smadzelonka@gmail.com>
2021-03-07 18:44:12 -0800

    starting on messages building out core layout

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -25,12 +25,10 @@ function NavBar() {
       <Container className="navbar-body">
         <NavBarItem icon={faHome} route="/feed" name="Home" />
         <NavBarItem icon={faHashtag} route="/feed" name="Explore" />
-
         <NavBarItem icon={faBell} route="/" name="Notifications" />
-        <NavBarItem icon={faEnvelope} route="/" name="Messages" />
+        <NavBarItem icon={faEnvelope} route="/messages" name="Messages" />
         <NavBarItem icon={faBookmark} route="/" name="Bookmark" />
         <NavBarItem icon={faListUl} route="/" name="Lists" />
-
         <NavBarItem
           icon={faUser}
           route={`/tweets/profile/${user[0] && user[0].id}`}
---
+++ frontend/src/components/messages/MessagesContainer.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavBar from "../../components/NavBar/NavBar";
+import "./MessagesContainer.css";
+
+const MessagesContainer = ({ user, body, messages }) => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <NavBar {...user} />
+        </Col>
+        <Col>
+          <Navbar
+            sticky="top"
+            expand="lg"
+            variant="light"
+            bg="light"
+            className="StickyNav"
+          >
+            <Navbar.Brand href="#">Messages</Navbar.Brand>
+            {/* Messages Icon */}
+            <FontAwesomeIcon
+              className="image-icon StickyNavIcon messagesIcon"
+              icon={faEnvelope}
+              size="1x"
+            />
+          </Navbar>
+          {/* Search Bar */}
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="searchBarMessages"
+            />
+            {/* <Button variant="outline-info">Search</Button> */}
+          </Form>
+          {body}
+        </Col>
+        <Col md={6}>{messages}</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default MessagesContainer;
