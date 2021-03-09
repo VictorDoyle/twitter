3adf78fcd081ed59daa1bfdf5c8718134abdff93
smadzelonka <smadzelonka@gmail.com>
2021-03-08 17:10:13 -0800

    frontend token route is built but commented out on login page

--- frontend/src/pages/loginPage/LoginPage.js
+++ frontend/src/pages/loginPage/LoginPage.js
@@ -4,12 +4,30 @@ import Login from "../../components/loginPage/Login";
 import AuthModel from "../../models/auth";
 import { userState } from "../../recoil/atoms";
 import { useSetRecoilState } from "recoil";
+// import { useMutation, gql } from "@apollo/client";
+
+// const SigninMutation = gql`
+//   mutation SigninUserMutation(
+//     $signinUserEmail: String!
+//     $signinUserPassword: String!
+//   ) {
+//     signinUser(email: $signinUserEmail, password: $signinUserPassword)
+//   }
+// `;
 
 const LoginPage = ({ history }) => {
   const setUser = useSetRecoilState(userState);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const submitHandler = (e) => {
+  // const [login, { loading, error }] = useMutation(SigninMutation);
+
+  const submitHandler = async (e) => {
+    // await login({
+    //   variables: {
+    //     email: email,
+    //     password: password,
+    //   },
+    // });
     e.preventDefault();
     AuthModel.login({ email, password }).then((json) => {
       localStorage.setItem("uid", json.token);
