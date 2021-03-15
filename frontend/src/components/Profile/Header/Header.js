55a7636c432fae41b1441802747f5bfd6b03380f
victordoyle <victor-doyle@outlook.com>
2021-03-15 18:12:35 -0400

    pre merge

--- frontend/src/components/Profile/Header/Header.js
+++ frontend/src/components/Profile/Header/Header.js
@@ -33,6 +33,7 @@ function handleProfileEdit(event) {
 
     return (
 <>
+
 <Row className="profileHero">
 {/* header image behind profile photo */}
 <Image  src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22866%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20866%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_177df71d25f%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3A-apple-system%2CBlinkMacSystemFont%2C%26quot%3BSegoe%20UI%26quot%3B%2CRoboto%2C%26quot%3BHelvetica%20Neue%26quot%3B%2CArial%2C%26quot%3BNoto%20Sans%26quot%3B%2C%26quot%3BLiberation%20Sans%26quot%3B%2Csans-serif%2C%26quot%3BApple%20Color%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Emoji%26quot%3B%2C%26quot%3BSegoe%20UI%20Symbol%26quot%3B%2C%26quot%3BNoto%20Color%20Emoji%26quot%3B%2C%20monospace%3Bfont-size%3A43pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_177df71d25f%22%3E%3Crect%20width%3D%22866%22%20height%3D%22250%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22317.6796875%22%20y%3D%22145.25%22%3E866x250%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" fluid />
@@ -48,7 +49,7 @@ function handleProfileEdit(event) {
         Edit Profile
     </Button>
 
-
+    {currentUser ? 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header>
             <Button className="closeSettingsModal" onClick={handleClose}>
@@ -96,7 +97,7 @@ function handleProfileEdit(event) {
 
         </Modal.Body>
       
-      </Modal>
+      </Modal>  : <h1> no user to edit </h1>} 
 </Row>
 
 <Row>
